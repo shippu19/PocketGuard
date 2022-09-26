5778a94ff1a6363a896cf378e6aad3a87eb2710c
amansingh4u98 <amansingh199835@gmail.com>
2022-09-26 10:52:16 +0530

    Changes to admin controller and service

--- admin/admin.model.js
+++ admin/admin.model.js
@@ -7,20 +7,17 @@ const AdminSchema = new Mongoose.Schema({
     profilecompleted:{type:Boolean,default:false},
     dob:{type:Date,required:true},
     email:{type:String, unique:true,required:true
-    //     ,validate:{
-    //     validator:function(email){
-    //         var re = /\S+@S+\.\S+/;
-    //         return re.test(email);
-    //     }
-    // }
+
 },
     password:{type:String,required:true},
     creationdate:{type:String,default:new Date()},
     verified:{type:Boolean,default:false},
     accounttype:{type:String,required:true},
     accountnumber:{type:Number,required:true,unique:true},
-    ifsc:{type:String,required:true}
+    ifsc:{type:String,required:true},
+    accountbalance:{type:Number,required:true,default:500},
+    numberoftransactions:{type:Number,default:1}
 })
 
-const AdminModel = Mongoose.model("admin",AdminSchema) // users is a collection in db where all the data's of this module will be stored
+const AdminModel = Mongoose.model("admin",AdminSchema) 
 module.exports= AdminModel;
--- admin/index.js
+++ admin/index.js
@@ -1,5 +1,7 @@
 const Express = require("express");
 const router = Express.Router();
+const UserController = require('./admin.controller')
 
+router.post('/register', UserController.signup)
 
 module.exports = router;
